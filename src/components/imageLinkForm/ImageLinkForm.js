ce0c6dfcc7fb46157f23900c9b3eee218bc43831
samuel ochaba <sammymadgenius@gmail.com>
2019-04-27 01:56:06 +0100

    adding initial files

---
+++ src/components/imageLinkForm/ImageLinkForm.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './imageLinkForm.css';
+
+
+const ImageLinkForm = ({onInputChange, onButtonSubmit}) => {
+    return (
+        <div>
+            <p className='f3'>
+                {'this magic brain will detect faces in your picture. give it a try'}
+            </p>
+            <div className="center">
+                <div className='form center pa4 br3 shadow-5'>
+                    <input type="text" className='f4 w-70 pa2 center' onChange={onInputChange}/>
+                    <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple' onClick={onButtonSubmit}>Detect</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ImageLinkForm;
